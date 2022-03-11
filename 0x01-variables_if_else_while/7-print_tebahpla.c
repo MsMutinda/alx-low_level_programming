8c21da5ac7a394a007e8cf41ddb5b7b0f4066993
MsMutinda <MsMutinda@users.noreply.github.com>
2022-03-12 01:39:40 +0300

    sjdxanajksdwuidj

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+/*
+ * Using putchar function to disply alphabet in reverse
+*/
+#include <stdio.h>
+/**
+ * main - block code
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	int alph;
+
+	for (alph = 'z'; alph >= 'a'; alph--)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
