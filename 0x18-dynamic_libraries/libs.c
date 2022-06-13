74106cced8e19a944c383cb00a12e1811c7f3338
Juliet <MsMutinda@users.noreply.github.com>
2022-06-13 19:53:26 +0300

    dynamic libs d

---
+++ 0x18-dynamic_libraries/libs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+int _putchar(char c) { return (1); }
+
+int _islower(int c) { return (1); }
+
+int _isalpha(int c) { return (1); }
+
+int _abs(int n) { return (1); }
+
+int _isupper(int c) { return (1); }
